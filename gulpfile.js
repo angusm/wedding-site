c45a6baf4dfb3b17cac4f388a38606ee604e9600
Angus MacDonald <angusamacdonald@hotmail.com>
2017-12-16 17:24:37 -0500

    Stubbed out for copy

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const gulp = require('gulp');
+
+const babelify = require('babelify');
+const browserify = require('browserify');
+const buffer = require('vinyl-buffer');
+const sass = require('gulp-sass');
+const source = require('vinyl-source-stream');
+const sourcemaps = require('gulp-sourcemaps');
+
+function handleError(err) {
+  console.log(err);
+  this.emit('end');
+}
+
+gulp.task('js', () => {
+  return browserify({entries: './source/js/main.js', debug: true})
+    .transform("babelify", {
+      presets: ["es2015"],
+    })
+    .on('error', handleError)
+    .bundle()
+    .on('error', handleError)
+    .pipe(source('main.min.js'))
+    .on('error', handleError)
+    .pipe(buffer())
+    .on('error', handleError)
+    .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('css', () => {
+  return gulp.src('./source/sass/main.min.sass')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .on('error', handleError)
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('default', ['css', 'js']);
