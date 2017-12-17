2a34979ecdd0f6f807643f269d8accafa195c9fb
Angus MacDonald <angusamacdonald@hotmail.com>
2017-12-17 02:25:57 -0500

    Initial styling and content

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,4 @@
-console.log('HIHIHI');
+const getCurrentAnchorByCenter = require('toolbox/components/deep-link-by-scroll/get-current-anchor-by-center');
+const DeepLinkByScroll = require('toolbox/components/deep-link-by-scroll/base');
+
+new DeepLinkByScroll(getCurrentAnchorByCenter, false);
